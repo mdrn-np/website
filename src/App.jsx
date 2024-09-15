d25f86bc5b205d37b3b4fe994909b7bfd12a050b
Manish Bagale <manish.bagale3@gmail.com>
2024-09-16 00:19:23 +0545

    created a new component proejcts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Hero from "./components/hero/hero";
 import ParticlesComponent from "./components/heroConfig/particles_Config";
 import NavBar from "./components/navBar";
+import Projects from "./components/projects";
 
 function App() {
   return (
     <>
       <NavBar />
-      <ParticlesComponent id="particles"/>
-      <Hero/>
+      <ParticlesComponent id="particles" />
+      <Hero />
+      <Projects />
     </>
   );
 }
---
+++ src/components/projects.jsx
@@ -0,0 +1,11 @@
+import { space } from "postcss/lib/list";
+import { project } from "../data/data.js";
+import { FaArrowRight } from "react-icons/fa";
+
+export default function Projects() {
+  return (
+    <div>
+      <h1 className="text-center heading font-semibold my-4">Our Projects</h1>
+    </div>
+  );
+}
