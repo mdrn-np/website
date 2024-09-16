1a5ffd242a627b982d87a5e274aaa63323f818c0
gauravphuyal <phuyalgaurav90@gmail.com>
2024-09-16 07:27:57 +0545

    add image in card ,add data & mapping.

--- src/components/cardSection.jsx
+++ src/components/cardSection.jsx
@@ -1,21 +1,28 @@
+import { data } from "../data/data";
 import CardDetail from "./cardDetail";
 
-
-export default function CardSection(){
-  return(
+export default function CardSection() {
+  return (
     <div>
-      <section className=" pb-[300px] py-16">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="text-center">
-                    <h1 className="text-3xl font-extrabold text-accent">What We Offer</h1>
-              </div>
-              <div className="mt-32 grid grid-cols-1 gap-8 md:grind-cols-2 lg:grid-cols-3">
-                  <CardDetail title="Uno" text="Something we do no1"/>
-                  <CardDetail title="Dos" text="Something we do no2 "/>
-                  <CardDetail title="Tres" text="Something we do no3 "/>
-              </div>
+      <section className="pb-[300px] py-16">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h1 className="text-3xl font-extrabold text-accent">
+              What We Offer
+            </h1>
           </div>
+          <div className="mt-32 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            {data.map((item) => (
+              <CardDetail
+                key={item.id}
+                title={item.title}
+                text={item.text}
+                image={item.image}
+              />
+            ))}
+          </div>
+        </div>
       </section>
     </div>
-  )
+  );
 }
