2346e0c1c477046592c1cc2702d0087ae1eab9dc
gauravphuyal <phuyalgaurav90@gmail.com>
2024-09-18 08:51:52 +0545

    refactor navbar & hero, upgrade vite

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -6,8 +6,8 @@ export default function Hero() {
     <div class="grid place-items-center h-[30rem]">
       <div class=" p-4 ">
         <h1 className="heading font-bold text-center">
-          <span>Tech For Tomorrow </span>
-          <span className="2">Actions for Today</span>
+          <span>Tech For Tomorrow</span>
+          <span className="2"> Actions for Today</span>
         </h1>
         <p className="sub-heading text-center">Todo: add about me</p>
       </div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import { navLinks } from "../../data/data";
-import logo from "../../assets/mdrn-logo.png";
+import logo from "../../assets/mdrn-logo-side.png";
 import { useState, useEffect } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoCloseOutline } from "react-icons/io5";
@@ -31,23 +31,21 @@ export default function NavBar() {
 
   return (
     <div
-      className={`fixed left-1/2 transform -translate-x-1/2 w-11/12 max-w-7xl z-50 transition-all duration-300 ${
+      className={`fixed left-1/2 transform -translate-x-1/2 w-11/12 max-w-7xl z-50 transition-all duration-300 opacity-[0.98] ${
         scrolled
-          ? "bg-[#B6BBC2] shadow-lg rounded-2xl p-2 top-4 border border-borderSoft translate-y-6 outline-0 !py-3 lg:!py-3.5 pr-3 pl-4 sm:pl-6 !bg-fill lg:!w-[940px] 2xl:!w-[1040px]"
-          : "bg-transparent top-0 border border-transparent py-4 sm:py-5 xl:py-6"
+          ? "bg-[#F5F6F6] shadow-lg rounded-2xl p-2 top-4 border border-borderSoft translate-y-6 outline-0 !py-1.5 lg:!py-2 pr-3 pl-4 sm:pl-6 !bg-fill lg:!w-[940px] 2xl:!w-[1040px] h-13"
+          : "bg-transparent top-0 border border-transparent py-2 sm:py-3 xl:py-4 h-13"
       }`}
     >
       <div
         className={`flex items-center justify-between text-xl transition-all duration-300 ${
-          scrolled ? "gap-1 p-2" : "gap-6 p-4"
+          scrolled ? "gap-1 p-2" : "gap-3 p-2"
         }`}
       >
         <img
           src={logo}
           alt="logo's png"
-          className={`max-h-20 w-20 flex-initial object-contain cursor-pointer hover:animate-jiggle transition-all duration-300 ${
-            scrolled ? "max-h-16 w-16" : "max-h-20 w-20"
-          }`}
+          className={`h-14 w-31 flex-initial object-contain cursor-pointer hover:animate-jiggle transition-all duration-300`}
           onClick={() => (window.location.href = "#home")}
         />
         <ul
@@ -72,14 +70,18 @@ export default function NavBar() {
           <button
             className={`rounded-md order-last transition-all duration-300 transform ${
               scrolled
-                ? "bg-gradient-to-r shadow-md from-indigo-900 to-violet-700 py-2.5 px-6 text-md items-center gap-2.5 cursor-pointer transition-backgroundColor duration-300 ease-in-out disabled:contrast-75 border text-primary focus:border bg-surface border-borderSoft hover:contrast-75 hidden sm:flex ml-auto lg:ml-0 w-40 h-12"
-                : "bg-primary  shadow-md group py-[9px] px-6 rounded-md text-md items-center gap-2.5 cursor-pointer transition-backgroundColor duration-300 ease-in-out disabled:contrast-75 border text-black focus:border bg-surface border-borderSoft hover:contrast-75 hidden sm:flex ml-auto lg:ml-0 w-40 h-12"
+                ? "bg-gradient-to-r from-indigo-900 to-violet-700 py-2 px-4 text-md items-center gap-2 cursor-pointer transition-backgroundColor duration-300 ease-in-out disabled:contrast-75 border text-white focus:border bg-surface border-borderSoft hover:contrast-75 hidden sm:flex ml-auto lg:ml-0 w-36 h-10"
+                : "bg-white group py-2 px-4 rounded-md text-md items-center gap-2 cursor-pointer transition-backgroundColor duration-300 ease-in-out disabled:contrast-75 border text-black focus:border bg-surface border-borderSoft hover:contrast-75 hidden sm:flex ml-auto lg:ml-0 w-36 h-10"
             }`}
           >
             <span className="transition-all duration-300 transform group-hover:mr-1 font-semibold">
               Join us
             </span>
-            <span className="transition-all duration-300 transform group-hover:ml-1">
+            <span
+              className={`transition-all duration-300 transform ${
+                scrolled ? "hover:ml-1" : "group-hover:ml-1"
+              }`}
+            >
               →
             </span>
           </button>
