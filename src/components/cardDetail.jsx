8cc2a33dd2736bccc7baa41918a4639bfaf4e26a
Saurav <sauravgamer441@gmail.com>
2024-09-18 21:01:01 +0545

    rework cards design

--- src/components/cardDetail.jsx
+++ src/components/cardDetail.jsx
@@ -1,21 +1,19 @@
+import Tilt from 'react-parallax-tilt';
+
 export default function CardDetail({ title, text, image }) {
   return (
-    <div className="relative group rounded-lg shadow-sm bg-gray-200 cursor-pointer p-4 sm:p-8 overflow-hidden">
-      <div className="absolute inset-0 bg-transparent group-hover:bg-gradient-to-br from-additional to-transparent group-hover:opacity-50 opacity-50 transition-all duration-300"></div>
+    <Tilt glareBorderRadius='8px' glareEnable glareColor='#3C1874' tiltMaxAngleX={"12"} tiltMaxAngleY={"12"}>
+    <div className="relative group rounded-lg shadow-lg bg-gray-400 bg-clip-padding bg-opacity-10  backdrop-filter backdrop-blur-sm  cursor-pointer p-4 sm:p-8 overflow-hidden">
       <div className="flex flex-col items-center space-y-4">
-        <div className="w-20 h-20 bg-gray-100 rounded-md overflow-hidden">
-          <img
-            src={image}
-            alt="Insert Image"
-            className="w-full h-full object-cover"
-            style={{ aspectRatio: "1 / 1" }}
-          />
+        <div className="w-20 h-20 shadow-md rounded-md overflow-hidden">
+          <img src={image} alt="logos here" className='p-3'/>
         </div>
         <div className="text-center">
           <h3 className="text-lg font-medium text-gray-900">{title}</h3>
-          <p className="mt-2 text-sm text-britrSec">{text}</p>
+          <p className="mt-2 text-sm text-gray-400">{text}</p>
         </div>
       </div>
     </div>
+    </Tilt>
   );
 }
