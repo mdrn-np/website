42304670632feba21af8b4ae29a7a718ee039542
gauravphuyal <phuyalgaurav90@gmail.com>
2024-10-08 10:32:57 +0545

    fuck git and fuck my life

--- src/components/sponsers/sponserDetail.jsx
+++ src/components/sponsers/sponserDetail.jsx
@@ -1,14 +1,20 @@
+import SponserIcon from "./sponsersicons.jsx";
+import Marquee from "react-fast-marquee";
+import "./animation.css";
 
-import SponserIcon from './sponsersicons.jsx'
-import './animation.css'
-
-export default function SponserDetail(){
-    return(
-        <div className="w-[100%] h-fit flex items-center justify-center flex-col">
-            <div className="vignette flex w-[75rem] select-none overflow-hidden">
-                <SponserIcon/>
-                <SponserIcon/>
-            </div>
-        </div>
-    )
+export default function SponserDetail() {
+  return (
+    <div className="w-full h-fit flex items-center justify-center flex-col">
+      <div className="vignette flex w-full sm:w-3/4 lg:w-1/2 select-none overflow-hidden overflow-y-hidden">
+        <Marquee
+          gradient={true}
+          gradientWidth={50} // Default gradient width
+          pauseOnHover={true}
+          speed={10}
+        >
+          <SponserIcon />
+        </Marquee>
+      </div>
+    </div>
+  );
 }
