599b730c64b6546af035642b74929fdbbd994d94
gauravphuyal <phuyalgaurav90@gmail.com>
2024-10-08 10:16:12 +0545

    Revert and use Marquee

--- src/components/sponsers/sponserDetail.jsx
+++ src/components/sponsers/sponserDetail.jsx
@@ -1,14 +1,15 @@
+import SponserIcon from "./sponsersicons.jsx";
+import "./animation.css";
+import Marquee from "react-fast-marquee";
 
-import SponserIcon from './sponsersicons.jsx'
-import './animation.css'
-
-export default function SponserDetail(){
-    return(
-        <div className="w-[100%] h-fit flex items-center justify-center flex-col">
-            <div className="vignette flex w-[75rem] select-none overflow-hidden">
-                <SponserIcon/>
-                <SponserIcon/>
-            </div>
-        </div>
-    )
+export default function SponserDetail() {
+  return (
+    <div className="w-[100%] h-fit flex items-center justify-center flex-col">
+      <div className="vignette flex w-[75rem] select-none overflow-hidden">
+        <Marquee>
+          <SponserIcon />
+        </Marquee>
+      </div>
+    </div>
+  );
 }
