7782924da4b6cdb9cfa59d700657eccb6e3e3623
Saurav <sauravgamer441@gmail.com>
2024-10-24 20:02:17 +0545

    Remove un-used cursor component

--- src/components/customCursor/customCursor.jsx
+++ src/components/customCursor/customCursor.jsx
@@ -7,16 +7,7 @@ export default function Cursor(){
     return(
         <>
         <AnimatedCursor
-  color="#fff"
-  innerSize={8}
-  outerSize={35}
-  innerScale={1}
-  outerScale={1.7}
-  outerAlpha={0}
-  outerStyle={{
-    mixBlendMode: 'exclusion'
-  }}
-/><AnimatedCursor
+    	
       innerSize={10}
       outerSize={10}
       color='193, 11, 111'
