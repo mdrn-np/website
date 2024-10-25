1f29ecbd71f041b09b582723b0e9cb951f46c04b
Manish Bagale <manish.bagale3@gmail.com>
2024-10-25 16:32:51 +0545

    added animations to members and testimonials

--- src/components/members/members.jsx
+++ src/components/members/members.jsx
@@ -66,6 +66,7 @@ export default function Members() {
           slideShadows: false,
         }}
         modules={[Autoplay, EffectCoverflow]}
+        className="members"
       >
         {memberList.map((memberInfo) => {
           const { name, icon, post, socials } = memberInfo;
